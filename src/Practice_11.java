956c8a91deb1d5a4523871f3f775589450a8a3ab
Alekzander <alekzanderor@gmail.com>
2020-03-28 18:12:07 +0400

    Initial commit

---
+++ src/Practice_11.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Practice_11 {
+
+    public static void main(String[] args)
+    {
+        //ввод массива
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input text: ");
+        String text=sc.nextLine();
+        System.out.println("Text: ");
+        System.out.println(text);
+
+    }
+}
